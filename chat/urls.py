1d174eba46e9f0c1159e6063c383902eaeae6889
AaronShenny <aaronshenny102.go@gmail.com>
2024-07-05 20:45:23 +0530

    Added App chatt

--- chat/urls.py
+++ chat/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
-    
+    path('',views.home),
     path('login/', views.login),
     path('chat/',views.chat)
 ]
--- chat/views.py
+++ chat/views.py
@@ -88,3 +88,6 @@ def chat(request):
         return HttpResponse('Chatting window')
     else:
         return redirect('/login/')
+
+def home(request):
+    return render(request, "home1.html")
